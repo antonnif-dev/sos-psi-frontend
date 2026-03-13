df921c3a01dace10b0a38a18539edf47af4a7804
antonnif-dev <antonnidev@gmail.com>
2026-03-13 19:50:00 -0300

    feat: .env com url do backend no render

--- src/services/api.js
+++ src/services/api.js
@@ -6,20 +6,15 @@ const api = axios.create({
 });
 
 api.interceptors.request.use(async(config)=>{
-
  const user = auth.currentUser;
 
  if(user){
-
   const token = await user.getIdToken();
-
   config.headers.Authorization =
    `Bearer ${token}`;
-
  }
 
  return config;
-
 });
 
 export default api;
